669f4f32928f11aace0cfcb037a72d351a22fbb1
maciek367 <maciek2baba2@gmail.com>
2023-03-01 19:53:04 +0100

    add 404 route

---
+++ routes/404.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "..", "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+module.exports = router
