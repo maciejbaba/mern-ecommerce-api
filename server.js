d80266d1c3d2702410ef2128545b9ec5a5a84a5c
maciek367 <maciek2baba2@gmail.com>
2023-03-01 17:45:37 +0100

    add css

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname), "/public"));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
