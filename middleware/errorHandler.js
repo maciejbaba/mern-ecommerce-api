11af850389da687b85030f98ef0a895b9409afda
maciek367 <maciek2baba2@gmail.com>
2023-03-01 21:23:14 +0100

    add to error logger loggingonly when origin exists

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,12 @@
 const { logEvents } = require("./logger");
 
 const errorHandler = (err, req, res, next) => {
-  //if (req.headers.origin) {
-  logEvents(
-    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
-    "errorLog.log"
-  );
+  if (req.headers.origin) {
+    logEvents(
+      `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+      "errorLog.log"
+    );
+  }
 
   console.log(err.stack);
 
