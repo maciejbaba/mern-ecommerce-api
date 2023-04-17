82f3c71f1b855ce61b4b4cc208eb2059974def0c
maciek367 <maciek2baba2@gmail.com>
2023-04-17 20:48:40 +0200

    add loginLimiter

---
+++ middleware/loginLimiter.js
@@ -0,0 +1,12 @@
+const rateLimit = require('express-rate-limit');
+const { logEvents } = require('./logger');
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 2 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'Too many request from this IP, please try again after 2 minutes',
+  handler: (req, res, next) => {
+    logEvents(`${req.ip} has exceeded the 5 requests per 2 minutes limit`, 'errorLog.log');
+    next();
+  },
+});
