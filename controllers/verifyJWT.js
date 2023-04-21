61c0e504b810fac5eb74ecacbe10d0d0eac9dcf3
maciek367 <maciek2baba2@gmail.com>
2023-04-21 17:01:19 +0200

    add verifyJWT middleware

---
+++ controllers/verifyJWT.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization; // good practice
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No authHeader" });
+  }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No Bearer token" });
+  }
+
+  const token = authHeader.split(" ")[1]; // "Bearer <token>" => ["Bearer", "<token>"] => "<token>"
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
