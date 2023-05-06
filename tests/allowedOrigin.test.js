bdd2626b6cf8e43a5d1936525b512b9c3949c2e7
maciek367 <maciek2baba2@gmail.com>
2023-05-06 22:49:25 +0200

    add more tests

--- tests/allowedOrigin.test.js
+++ tests/allowedOrigin.test.js
@@ -8,7 +8,15 @@ test("allowedOrigin is not empty", () => {
   expect(allowedOrigin.length).toBeGreaterThan(0);
 });
 
-test("allowedOrigin is either localhost or vercel", () => {
-  expect(allowedOrigin).toMatch(/localhost|vercel/);
+test("allowedOrigin is localhost when process.env.NODE_ENV equals development", () => {
+  process.env.NODE_ENV = "development";
+  const allowedOrigin = require("../config/allowedOrigin");
+  expect(allowedOrigin).toBe("http://localhost:3000");
 });
 
+test("allowedOrigin is vercel when process.env.NODE_ENV equals production", () => {
+  jest.resetModules();
+  process.env.NODE_ENV = "production";
+  const allowedOrigin = require("../config/allowedOrigin");
+  expect(allowedOrigin).toBe("https://mern-ecommerce-beta.vercel.app");
+});
