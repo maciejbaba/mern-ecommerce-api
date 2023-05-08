8cf7aeb5ba9ff627923e7a6ee6cda93b9aff35ae
maciek367 <maciek2baba2@gmail.com>
2023-05-08 19:02:32 +0200

    change to vite's default port

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -3,7 +3,7 @@ let allowedOrigin;
 if (process.env.NODE_ENV === "production") {
   allowedOrigin = "https://mern-ecommerce-beta.vercel.app";
 } else {
-  allowedOrigin = "http://localhost:3000"; // dev mode
+  allowedOrigin = "http://localhost:5173"; // dev mode
 }
 
 module.exports = allowedOrigin;
