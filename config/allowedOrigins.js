12ae9842ade1b1ce7d6968578ebd1bd31d75e92a
maciek367 <maciek2baba2@gmail.com>
2023-06-15 22:58:26 +0200

    update comment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ let allowedOrigins;
 if (process.env.NODE_ENV === "production") {
   allowedOrigins = ["https://mern-ecommerce-beta.vercel.app"];
 } else {
-  allowedOrigins = ["http://localhost:5173", "http://localhost:3500"]; // dev mode
+  allowedOrigins = ["http://localhost:5173", "http://localhost:3500"]; // dev mode, 5173 is the port of the frontend, 3500 is the port of the backend server its needed here for the tests
 }
 
 module.exports = allowedOrigins;
