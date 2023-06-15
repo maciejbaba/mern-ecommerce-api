7ee169194bc7ddcfab6cd44887a9641b04955adc
maciek367 <maciek2baba2@gmail.com>
2023-06-16 00:50:42 +0200

    change messages

--- middleware/requestLimiter.js
+++ middleware/requestLimiter.js
@@ -2,19 +2,19 @@ const rateLimit = require("express-rate-limit");
 const { logEvents } = require("./logger");
 
 const requestLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 5 minutes
-  max: 20, // limit each IP to 5 requests per windowMs
-  message: "Too many request from this IP, please try again after 5 minutes",
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 20, // limit each IP to 20 requests per windowMs
+  message: "Too many request from this IP, please try again after 1 minutes",
   handler: (req, res, next) => {
     logEvents(
-      `${req.ip} has exceeded the 5 requests per 5 minutes limit\n`,
+      `${req.ip} has exceeded the 20 requests per 1 minutes limit\n`,
       "errorLog.log"
     );
     res
       .status(429)
       .json({
         message:
-          "Too many request from this IP, please try again after 5 minutes",
+          "Too many request from this IP, please try again after 1 minutes",
       });
   },
 });
