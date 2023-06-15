6c4f74d497e35dc6dd60b51601183e31a579f84c
maciek367 <maciek2baba2@gmail.com>
2023-06-15 22:54:27 +0200

    fix comments

--- tests/allowedOrigins.test.js
+++ tests/allowedOrigins.test.js
@@ -1,20 +1,20 @@
 const allowedOrigins = require("../config/allowedOrigins");
 
-test("allowedOrigin is a string", () => {
+test("allowedOrigins is a object", () => {
   expect(typeof allowedOrigins).toBe("object");
 });
 
-test("allowedOrigin is not empty", () => {
+test("allowedOrigins is not empty", () => {
   expect(allowedOrigins.length).toBeGreaterThan(0);
 });
 
-test("allowedOrigin is localhost when process.env.NODE_ENV equals development", () => {
+test("allowedOrigins are localhost when process.env.NODE_ENV equals development", () => {
   process.env.NODE_ENV = "development";
   const allowedOrigins = require("../config/allowedOrigins");
   expect(allowedOrigins.includes("http://localhost:5173" || "http://localhost:3500")).toBe(true);
 });
 
-test("allowedOrigin is vercel when process.env.NODE_ENV equals production", () => {
+test("allowedOrigins is vercel when process.env.NODE_ENV equals production", () => {
   jest.resetModules();
   process.env.NODE_ENV = "production";
   const allowedOrigins = require("../config/allowedOrigins");
