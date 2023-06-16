42af10f58cd28902362e076edd5d7febf35f2aaf
maciek367 <maciek2baba2@gmail.com>
2023-06-17 00:41:39 +0200

    change request limiter max amount

--- middleware/requestLimiter.js
+++ middleware/requestLimiter.js
@@ -3,7 +3,7 @@ const { logEvents } = require("./logger");
 
 const requestLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 2000, // limit each IP to 20 requests per windowMs
+  max: 20, // limit each IP to 20 requests per windowMs
   message: "Too many request from this IP, please try again after 1 minutes",
   handler: (req, res, next) => {
     logEvents(
