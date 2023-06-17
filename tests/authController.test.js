52069715bb9135a94574dd6f929d718807fa1137
maciek367 <maciek2baba2@gmail.com>
2023-06-17 21:50:05 +0200

    properly describe

--- tests/authController.test.js
+++ tests/authController.test.js
@@ -7,47 +7,49 @@ const loginRequest = async (body) => {
 };
 
 describe("Auth Controller", () => {
-  test("should return 200 when login is successful", async () => {
-    const body = {
-      username: "test",
-      password: "test",
-    };
-    const response = await loginRequest(body);
-    expect(response.status).toBe(200);
-  });
-  test("should return 400 when username is missing", async () => {
-    const body = {
-      password: "test",
-    };
-    const response = await loginRequest(body);
-    expect(response.status).toBe(400);
-  });
-  test("should return 400 when password is missing", async () => {
-    const body = {
-      username: "test",
-    };
-    const response = await loginRequest(body);
-    expect(response.status).toBe(400);
-  });
-  test("should return 400 when both username and password are missing", async () => {
-    const body = {};
-    const response = await loginRequest(body);
-    expect(response.status).toBe(400);
-  });
-  test("should return 401 when password is incorrect", async () => {
-    const body = {
-      username: "test",
-      password: "wrong",
-    };
-    const response = await loginRequest(body);
-    expect(response.status).toBe(401);
-  });
-  test("should return 401 when username is incorrect", async () => {
-    const body = {
-      username: "wrong",
-      password: "test",
-    };
-    const response = await loginRequest(body);
-    expect(response.status).toBe(401);
+  describe("POST /login", () => {
+    test("should return 200 when login is successful", async () => {
+      const body = {
+        username: "test",
+        password: "test",
+      };
+      const response = await loginRequest(body);
+      expect(response.status).toBe(200);
+    });
+    test("should return 400 when username is missing", async () => {
+      const body = {
+        password: "test",
+      };
+      const response = await loginRequest(body);
+      expect(response.status).toBe(400);
+    });
+    test("should return 400 when password is missing", async () => {
+      const body = {
+        username: "test",
+      };
+      const response = await loginRequest(body);
+      expect(response.status).toBe(400);
+    });
+    test("should return 400 when both username and password are missing", async () => {
+      const body = {};
+      const response = await loginRequest(body);
+      expect(response.status).toBe(400);
+    });
+    test("should return 401 when password is incorrect", async () => {
+      const body = {
+        username: "test",
+        password: "wrong",
+      };
+      const response = await loginRequest(body);
+      expect(response.status).toBe(401);
+    });
+    test("should return 401 when username is incorrect", async () => {
+      const body = {
+        username: "wrong",
+        password: "test",
+      };
+      const response = await loginRequest(body);
+      expect(response.status).toBe(401);
+    });
   });
 });
