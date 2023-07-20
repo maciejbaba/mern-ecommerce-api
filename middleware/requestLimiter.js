2ed8ebc259d22e1f934014fdfa38eb1bd617fbe6
maciek367 <maciek2baba2@gmail.com>
2023-07-20 21:39:40 +0200

    fix typo in requestLimiter

--- middleware/requestLimiter.js
+++ middleware/requestLimiter.js
@@ -3,19 +3,16 @@ const { logEvents } = require("./logger");
 
 const requestLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: process.env.NODE_ENV = "development" ? 2000 : 20, // limit each IP to 20 requests per windowMs
+  max: process.env.NODE_ENV == "development" ? 2000 : 20, // limit each IP to 20 requests per windowMs
   message: "Too many request from this IP, please try again after 1 minute",
   handler: (req, res, next) => {
     logEvents(
       `${req.ip} has exceeded the 20 requests per 1 minute limit\n`,
       "errorLog.log"
     );
-    res
-      .status(429)
-      .json({
-        message:
-          "Too many request from this IP, please try again after 1 minute",
-      });
+    res.status(429).json({
+      message: "Too many request from this IP, please try again after 1 minute",
+    });
   },
 });
 
